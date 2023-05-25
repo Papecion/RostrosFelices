eac84f7f7c92effc04abba03d5c62e33cd13feed
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-05-25 15:39:04 -0500

    Modificacion en el modelo servicio

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public int Documento { get; set; }
+        public string Documento { get; set; }
 
         ICollection<Dato> Datos { get; set; } = default!;
 
